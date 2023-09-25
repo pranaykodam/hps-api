c630331bf317890895ab520615fe14bb13c92004
pranaykumarkodam <pranay.k@blocmatrix.com>
2023-09-25 18:42:48 +0530

    update start file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const App = express();
 App.use(express.json());
+App.get("/", (req, res) => {
+  res.send({ status: true, message: "server is running" });
+});
 App.listen(8000, (req, res) =>
   console.log("Server is running at http://localhost:8000")
 );

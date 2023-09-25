c609a03ac3b347e1bd3ac222964cf7f6462408ec
pranaykumarkodam <pranay.k@blocmatrix.com>
2023-09-25 18:51:17 +0530

    update

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const fs = require("fs");
+
+let rawdata = fs.readFileSync("hps-data.json");
+let data = JSON.parse(rawdata);
 const App = express();
 App.use(express.json());
 App.get("/", (req, res) => {
-  res.send({ status: true, message: "server is running" });
+  res.send({ status: true, message: "server is running", data });
 });
 App.listen(8000, (req, res) =>
   console.log("Server is running at http://localhost:8000")
